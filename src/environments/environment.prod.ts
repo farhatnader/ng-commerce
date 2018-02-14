b2b9be75a49af6aa735e45d7d8806002f0102156
Farhat Nader <farhatmnader@gmail.com>
2018-02-13 23:29:52 -0500

    setup firebase and add navbar component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4FQt-EWXFiJyuSXQAhrOfbjUQSEg4JyM",
+    authDomain: "ng-commerce-farhat.firebaseapp.com",
+    databaseURL: "https://ng-commerce-farhat.firebaseio.com",
+    projectId: "ng-commerce-farhat",
+    storageBucket: "",
+    messagingSenderId: "798425234937"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC4FQt-EWXFiJyuSXQAhrOfbjUQSEg4JyM",
+    authDomain: "ng-commerce-farhat.firebaseapp.com",
+    databaseURL: "https://ng-commerce-farhat.firebaseio.com",
+    projectId: "ng-commerce-farhat",
+    storageBucket: "",
+    messagingSenderId: "798425234937"
+  }
 };
