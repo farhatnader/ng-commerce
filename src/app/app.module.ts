914be5e50b12521069180be919bcf69b63ea5462
Farhat Nader <farhatmnader@gmail.com>
2018-02-18 13:09:43 -0500

    encapsulate authentication within service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
 
+import { AuthService } from './auth.service';
+
 
 @NgModule({
   declarations: [
@@ -52,7 +54,7 @@ import { LoginComponent } from './login/login.component';
       { path: 'admin/orders', component: AdminOrdersComponent }
     ])
   ],
-  providers: [],
+  providers: [ AuthService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -9,13 +7,12 @@ import * as firebase from 'firebase';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
+  constructor(public auth: AuthService) {
+    
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 }
