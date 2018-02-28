a067660b4c2f3f01d6c09ddf20d7387c006b710f
Farhat Nader <farhatmnader@gmail.com>
2018-02-27 23:50:41 -0500

    implement filter search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit , OnDestroy {
+  products: any[]; // should be array to implement filter feature
+  filteredProducts: any[];
+  subscription: Subscription; // to point to an open subscription
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  // if query typed in, use js filter method to reset filteredProducts
+  // to products including the query string
+  // else (user deletes query) filteredProducts should equal original list
+  filter(query: string) {
+    this.filteredProducts = (query) ? 
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  // only unsubscribe when component is destroyed
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit() {
