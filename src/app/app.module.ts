38469cd7779abd45d0927b2716b9fe13ff0deddc
Farhat Nader <farhatmnader@gmail.com>
2018-03-03 23:59:59 -0500

    implement products page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,7 @@ import { ProductService } from './product.service';
     AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: ProductsComponent },
       { path: 'login', component: LoginComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
