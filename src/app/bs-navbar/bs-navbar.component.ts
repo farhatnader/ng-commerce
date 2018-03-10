2a0435d7b2bef56ee1a98466d6e754de6d567391
Farhat Nader <farhatmnader@gmail.com>
2018-03-10 17:42:40 -0500

    add cart count to navbar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
 
   // subscribe to authService's appUser$ to replace asyc pipe in template
   // using async pipe with a siwtchmap can lead to infinite loop or rendering
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private cartService: ShoppingCartService) {
     auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  }
+
+  async ngOnInit() {
+    this.cart$ = await this.cartService.getCart();
   }
 
   logout() {
